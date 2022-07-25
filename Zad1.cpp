1fa8d77f1dd4c144c11fbdac7b1e4af2e2323a83
mikolajczykmateusz <mikolajczyk.mateusz96@gmail.com>
2022-07-25 21:07:53 +0200

    first commit

---
+++ Zad1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <string>
+ 
+using namespace std;
+using std::fstream;
+ 
+int main(int argc, char *argv[]) {
+  int n;
+  cout<<"Liczba losowanych punktow n = ";
+  cin>>n;
+  srand(time(NULL));
+  float x,y;
+  int k=0;
+  for(int i=1; i<=n; i++){
+    x=rand()/float(RAND_MAX);
+    y=rand()/float(RAND_MAX);
+    if(x*x+y*y<=1) k++;
+  };
+  float p=4.*k/n;
+  cout<<"pi = "<<setprecision(5)<<p<<endl;
+  
+  fstream plik;
+  plik.open("dane.txt", ios::out);
+  plik<<n<<endl;
+  plik<<p<<endl;
+  plik.close();
+  
+  char c;
+  cin>>c;
+}
